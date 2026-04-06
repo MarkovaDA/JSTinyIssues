72158c52e7fd83ac22deb74e92b235ca3084c981
darya_markova <darya.markova.95@mail.ru>
2026-04-06 13:45:27 +0300

    callbackify функция

--- async/callbackify.js
+++ async/callbackify.js
@@ -8,13 +8,55 @@ async function sum(a, b) {
 
 function callbackify(originalFunction) {
   return function(...args) {
-    // последний аргумент - callback, извлекаем его
+  
     const callback = args.pop();
 
-     originalFunction(...args).then(result => {
+    originalFunction(...args).then(result => {
       callback(null, result);
-     }).catch(error => {
-        callback(error);
-     })}
+    }).catch(error => {
+      callback(error);
+    })}
 }
 
+function callbackifyNew(originalFunction) {
+  if (typeof originalFunction !== 'function') {
+    throw new TypeError('The first argument must be a function');
+  }
+
+  return function(...args) {
+    const lastArg = args[args.length - 1];
+    
+    if (typeof lastArg !== 'function') {
+      throw new TypeError('The last argument must be a callback function');
+    }
+  }
+
+  // коллбэк - это последний аргумент
+  const callback = args.pop();
+
+  Promise.resolve()
+    .then(() => originalFunction(...args))
+    .then(result => {
+      try {
+        callback(null, result)
+      } catch(callbackError) {
+        throw callbackError;
+      }
+    })
+    .catch(error => {
+      try {
+        callback(error)
+      } catch (callbackError) {
+        throw callbackError;
+      }
+    })
+}
+
+failingCallback((err, result) => {
+  if (err) {
+    console.error('Error caught:', err.message); // Error caught: Something went wrong
+    return;
+  }
+  
+  console.log('Result:', result);
+});
